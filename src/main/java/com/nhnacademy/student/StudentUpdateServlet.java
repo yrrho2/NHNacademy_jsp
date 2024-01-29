a525fad6428a38a700492218a6f338fa2f82c173
yrrho2 <yrrho2@naver.com>
2024-01-29 17:50:15 +0900

    03-01-02-05

--- src/main/java/com/nhnacademy/student/StudentUpdateServlet.java
+++ src/main/java/com/nhnacademy/student/StudentUpdateServlet.java
@@ -27,8 +27,9 @@ public class StudentUpdateServlet extends HttpServlet {
         req.setAttribute("student",student);
 
         //todo forward : /student/register.jsp
-        RequestDispatcher rd = req.getRequestDispatcher("/student/register.jsp");
-        rd.forward(req,resp);
+//        RequestDispatcher rd = req.getRequestDispatcher("/student/register.jsp");
+//        rd.forward(req,resp);
+        req.setAttribute("view", "/student/register.jsp");
     }
 
     @Override
@@ -56,6 +57,7 @@ public class StudentUpdateServlet extends HttpServlet {
         studentRepository.update(student);
 
         //todo /student/view?id=student1 <-- redirect
-        resp.sendRedirect("/student/view?id="+id);
+//        resp.sendRedirect("/student/view?id="+id);
+        req.setAttribute("view", "/student/view?id="+id);
     }
 }
